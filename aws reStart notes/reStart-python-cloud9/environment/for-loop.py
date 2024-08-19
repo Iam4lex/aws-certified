fd4d8a60a8d07ae023f7c2941fa02b845be50f87
Alex <125695276+scriptkiddieke@users.noreply.github.com>
2024-08-19 11:45:08 +0300

    Initial Commit

--- aws reStart notes/reStart-python-cloud9/environment/for-loop.py
+++
@@ -1,3 +0,0 @@
-print("count to 10!")
-for x in range (0,11):
-    print(x)
--- aws reStart notes/reStart-python-cloud9/environment/loops.py
+++
@@ -1,5 +0,0 @@
-myMixedTypelist = [45, 290578, 1.02, True, "My dog is on the bed." , "45"]
-
-for item in myMixedTypeList:
-    print("{} is of the data type {}".format(item,type(item)))
-    
